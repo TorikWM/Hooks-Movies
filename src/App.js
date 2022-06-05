3e125989eca5086bef0ee5a99904d4f3a4977f7f
Malek Bouraoui <81751310+TorikWM@users.noreply.github.com>
2022-06-05 17:52:42 +0100

    A

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from "react-bootstrap";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
